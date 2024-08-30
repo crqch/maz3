88c303b6190fec41daaaf986c50121ce99fb3799
crqch <48185508+crqch@users.noreply.github.com>
2024-08-30 22:51:07 +0200

    Test IP on Vercel

--- src/app/_components/leaderboard.tsx
+++ src/app/_components/leaderboard.tsx
@@ -1,17 +1,22 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
+import { MdArrowDownward, MdArrowUpward } from "react-icons/md";
 import { api } from "~/trpc/react";
 
 export default function Leaderboard() {
-    const query = api.get.getLeaderboard.useQuery()
+    const [page, setPage] = useState<number>(0);
+
+    const query = api.get.getLeaderboard.useQuery(page)
    
-    return <div className="flex flex-col gap-y-4">
-        {query.data?.length === 0 && <p className="text-black/80">Be first to appear on the leaderboard!</p>}
-        {query.data?.map(score => <div key={"score-" + score.name + score.time} className="flex flex-row justify-between items-center p-2 bg-black text-white">
+    return <div className="flex flex-col gap-y-4 items-center">
+        {(query.data?.maxPage ?? 1) === 0 && <p className="text-black/80">Be first to appear on the leaderboard!</p>}
+        {page > 0 && <MdArrowUpward className="cursor-pointer text-black/50" onClick={() => setPage(page - 1)} />}
+        {query.data?.results.map(score => <div key={"score-" + score.name + score.time} className="flex w-full flex-row justify-between items-center p-2 bg-black text-white">
             <p className="font-bold">{score.time.toFixed(2)}</p>
             <p>{score.name}</p>
         </div>)}
+        {page < ((query.data?.maxPage ?? 20) - 1) && <MdArrowDownward className="cursor-pointer text-black/50" onClick={() => setPage(page + 1)} />}
         <p onClick={() => query.refetch()} className="text-black/80 cursor-pointer">Refresh</p>
     </div>
 }
--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -7,9 +7,21 @@ let leaderboard: {
   time: number
 }[] = []
 
+const lastRun: {
+  [key: string]: number
+} = {}
+
 const blacklist: {
   [key: string]: number
 } = {}
+
+const addBlacklist = (ip: string) => {
+  if (blacklist[ip] !== undefined) {
+    blacklist[ip]++
+  } else {
+    blacklist[ip] = 1
+  }
+}
 
 export const appRouter = createTRPCRouter({
   post: createTRPCRouter({
@@ -20,30 +32,38 @@ export const appRouter = createTRPCRouter({
       }))
       .mutation(({ input, ctx }): [number, string] => {
         const ip = (ctx.headers.get('x-forwarded-for') ?? '127.0.0.1').split(',')[0]
-        if (input.time < 7) return [1, "Couldn't upload the score because it's too fast!"]
-        if (input.name.length < 3 && input.name !== "") return [0, "The name is too short"]
-        if(ip !== undefined && blacklist[ip] !== undefined){
-          if(blacklist[ip] > 3) return [1, "You have been blacklisted"]
-        }
-        if (profanity.includes(input.name.toLowerCase())) {
-          if(ip !== undefined){
-            if(blacklist[ip] !== undefined){
-              blacklist[ip] ++
-            }else{
-              blacklist[ip] = 1
-            }
-          }
-          return [1, "This name contains profanity"]
-        }
-        leaderboard.push(input)
-        leaderboard = leaderboard.sort((a, b) => a.time - b.time)
-        return [1, "Successfully reported the score"];
+        return [1, ip ?? "none"]
+        // if (input.time < 7) return [1, "Couldn't upload the score because it's too fast!"]
+        // if (input.name.length < 3 && input.name !== "") return [0, "The name is too short"]
+        // if (ip !== undefined && blacklist[ip] !== undefined) {
+        //   if (blacklist[ip] > 3) return [1, "You have been blacklisted"]
+        // }
+        // if (ip !== undefined && lastRun[ip] !== undefined) {
+        //   if (new Date().getTime() - input.time * 1000 + lastRun[ip] > 0) {
+        //     addBlacklist(ip)
+        //     return [1, "Timestamps mismatch"]
+        //   }
+        // }
+        // if (profanity.includes(input.name.toLowerCase())) {
+        //   if (ip !== undefined) {
+        //     addBlacklist(ip)
+        //   }
+        //   return [1, "This name contains profanity"]
+        // }
+        // leaderboard.push(input)
+        // leaderboard = leaderboard.sort((a, b) => a.time - b.time)
+        // if (ip !== undefined) lastRun[ip] = new Date().getTime()
+        // return [1, "Successfully reported the score"];
       })
   }),
   get: createTRPCRouter({
     getLeaderboard: publicProcedure
-      .query(() => {
-        return leaderboard;
+      .input(z.number())
+      .query(({ input }) => {
+        return {
+          results: leaderboard.slice(input * 15, input * 15 + 15),
+          maxPage: Math.ceil(leaderboard.length / 15)
+        };
       })
   })
 });
